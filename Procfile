d5f828ee3fabf9cb33d5bcc0d1c084410203a3d4
Andrew Patton <andrew@acusti.ca>
2015-05-05 12:40:39 -0400

    Scaffold created by react-template
    Customized font-family in site.css and page definitions in
    redo/core.cljs

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: java $JVM_OPTS -cp target/redo.jar clojure.main -m redo.server
---
+++ env/prod/cljs/redo/prod.cljs
@@ -0,0 +1,7 @@
+(ns redo.prod
+  (:require [redo.core :as core]))
+
+;;ignore println statements in prod
+(set! *print-fn* (fn [& _]))
+
+(core/init!)
